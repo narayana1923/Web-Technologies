b95ca9a95eb0ee04c028c2ae8f8123c51c2a4a5a
Lakshmi Narayana Velayudam <lcchinnu@gmail.com>
2021-11-28 12:29:43 +0530

    week5 completed

--- week5/index.js
+++ week5/index.js
@@ -1,3 +1,58 @@
 $("button").click(function () {
   $(".container").toggle();
 });
+function showVideo(ele) {
+  $("#videoTag").attr("src", ele.getAttribute("src"));
+}
+var flag = false;
+function animer() {
+  if (flag) {
+    $("img")
+      .animate(
+        { width: "400px", height: "100px" },
+        {
+          duration: 1000,
+          easing: "linear",
+        }
+      )
+      .animate({ opacity: "50%" });
+    flag = !flag;
+    $("#vidSpan").animate(
+      { fontSize: "70px" },
+      { duration: 2000, complete: animer }
+    );
+  } else {
+    $("img")
+      .animate(
+        { width: "20px", height: "20px" },
+        {
+          duration: 1000,
+          easing: "linear",
+        }
+      )
+      .animate({ opacity: "100%" });
+    flag = !flag;
+    $("#vidSpan").animate(
+      { fontSize: "10px" },
+      { duration: 2000, complete: animer }
+    );
+  }
+}
+function areaAnimer() {
+  $(".area")
+    .animate(
+      { left: 0, width: "250px", height: "250px", top: 0 },
+      { duration: 1000, easing: "linear" }
+    )
+    .animate(
+      {
+        left: "1200px",
+        width: "50px",
+        height: "50px",
+        top: "60px",
+      },
+      { duration: 1000, easing: "linear", complete: areaAnimer }
+    );
+}
+animer();
+areaAnimer();
